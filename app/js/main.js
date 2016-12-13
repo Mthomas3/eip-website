9193a53372e8bec0de010f83a390ded3615a7273
thomasmartins <thomas.martins@epitech.eu>
2016-12-13 08:04:14 +0100

    start home page

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,8 @@
-console.log("bonjour")
+//todo different file
+
+$(function(){
+    $('.scroll-down').click(function(){
+        $('html, body').animate({scrollTop: $('section.content').offset().top}, 'slow')
+        return false
+    })
+})
