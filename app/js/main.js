700cc1027886149ee99c062f3c2b03402b772297
thomasmartins <thomas.martins@epitech.eu>
2016-12-29 16:44:05 +0100

    add v1 loader

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,11 @@
 //todo different files
 
+$(document).ready(function(){
+    setTimeout(function(){
+        $('body').addClass('loaded')
+    }, 3000)
+})
+
 class HandleSectionMenu{
 
     static HandleAction(section, scrolling){
